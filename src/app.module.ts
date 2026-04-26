388fd1771c5152474f48d4ff94cdaca7129f3f24
Alvaro Rivera <alvaro.regulo.rivera@gmail.com>
2026-04-26 09:28:41 -0400

    feat(upload): implementa servicio de subida a cloudinary con streams

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { EventsModule } from './events/events.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, RoomsModule, PrismaModule, EventsModule, WebhooksModule],
+  imports: [UsersModule, AuthModule, RoomsModule, PrismaModule, EventsModule, WebhooksModule, UploadModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
