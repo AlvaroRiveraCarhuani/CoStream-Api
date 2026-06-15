329736bb182b1328460261c7154d6a58c4a3cc84
Alvaro Rivera <alvaro.regulo.rivera@gmail.com>
2026-06-15 17:07:28 -0400

    fix:correcion de login

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: 'https://costream-api.onrender.com/api/auth/google/callback',
       scope: ['email', 'profile'],
     } as StrategyOptions);
     
